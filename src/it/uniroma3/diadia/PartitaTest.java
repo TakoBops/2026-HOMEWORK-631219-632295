0dd543473f39ef11efe5067d7cc2cb50d3d7e904
Arnel Pastor <nikkilenra.greentea@gmail.com>
2026-04-07 18:52:35 +0200

    Add files via upload

---
+++ src/it/uniroma3/diadia/PartitaTest.java
@@ -0,0 +1,31 @@
+package it.uniroma3.diadia;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PartitaTest {
+    private Partita partita;
+
+    @Before
+    public void setUp() {
+        partita = new Partita();
+    }
+
+    @Test
+    public void testIsFinitaCfuFiniti() {
+        partita.setCfu(0);
+        assertTrue(partita.isFinita());
+    }
+
+    @Test
+    public void testIsFinitaPartitaVinta() {
+        partita.setStanzaCorrente(partita.getStanzaVincente());
+        assertTrue(partita.isFinita());
+    }
+
+    @Test
+    public void testVintaFalseInizio() {
+        assertFalse(partita.vinta());
+    }
+}

58e6c87417da9196d0471d6622db4e715ade86ed
DavideDeVita3 <dav.devita@stud.uniroma3.it>
2026-05-05 18:00:50 +0200

    Create ComandoNonValido.java

---
+++ src/it/uniroma3/diadia/comandi/ComandoNonValido.java
@@ -0,0 +1,31 @@
+package it.uniroma3.diadia.comandi;
+
+import it.uniroma3.diadia.Partita;
+import it.uniroma3.diadia.IO;
+
+public class ComandoNonValido implements Comando {
+    private IO io;
+
+    @Override
+    public void esegui(Partita partita) {
+        this.io.mostraMessaggio("Comando non valido.");
+    }
+
+    @Override
+    public void setParametro(String parametro) {}
+
+    @Override
+    public String getNome() {
+        return "non valido";
+    }
+
+    @Override
+    public String getParametro() {
+        return null;
+    }
+
+    @Override
+    public void setIo(IO io) {
+        this.io = io;
+    }
+}

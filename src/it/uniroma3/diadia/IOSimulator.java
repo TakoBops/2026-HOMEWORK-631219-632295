abe602e062978bc85381c4810f63f818d2b150d1
DavideDeVita3 <dav.devita@stud.uniroma3.it>
2026-06-08 15:58:56 +0200

    Update IOSimulator.java

--- src/it/uniroma3/diadia/IOSimulator.java
+++ src/it/uniroma3/diadia/IOSimulator.java
@@ -1,15 +1,36 @@
 package it.uniroma3.diadia;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IOSimulator implements IO {
-    // Implementare array o liste per memorizzare righe lette e messaggi scritti
-    @Override
-    public void mostraMessaggio(String messaggio) {
-        // Salva il messaggio per i test
+    private List<String> righeDaLeggere;
+    private int indiceRighe;
+    private List<String> messaggiProdotti;
+
+    public IOSimulator(List<String> righeDaLeggere) {
+        this.righeDaLeggere = righeDaLeggere;
+        this.indiceRighe = 0;
+        this.messaggiProdotti = new ArrayList<>();
     }
 
     @Override
     public String leggiRiga() {
-        // Ritorna le righe "iniettate" dal test
-        return null; 
+        if (this.indiceRighe < this.righeDaLeggere.size()) {
+            String riga = this.righeDaLeggere.get(this.indiceRighe);
+            this.indiceRighe++;
+            return riga;
+        } else {
+            return null; // Nessun altro comando da leggere
+        }
+    }
+
+    @Override
+    public void mostraMessaggio(String messaggio) {
+        this.messaggiProdotti.add(messaggio);
+    }
+
+    public List<String> getMessaggiProdotti() {
+        return this.messaggiProdotti;
     }
 }

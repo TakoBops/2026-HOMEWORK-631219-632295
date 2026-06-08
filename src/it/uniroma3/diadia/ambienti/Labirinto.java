f81af7e818d5a73cdb73d6e5163a1350e3e4714b
DavideDeVita3 <dav.devita@stud.uniroma3.it>
2026-06-08 15:51:57 +0200

    Update Labirinto.java

--- src/it/uniroma3/diadia/ambienti/Labirinto.java
+++ src/it/uniroma3/diadia/ambienti/Labirinto.java
@@ -1,70 +1,23 @@
-package it.uniroma3.diadia.ambienti;[cite: 1]
-
-import it.uniroma3.diadia.attrezzi.Attrezzo;
+package it.uniroma3.diadia.ambienti;
 
 public class Labirinto {
 	
 	private Stanza stanzaIniziale;
 	private Stanza stanzaVincente;
-	private Stanza stanzaCorrente;
+
 	/**
-	 * Costruttore della classe Labirinto.
-	 * Chiama il metodo per generare le stanze e i loro collegamenti.
+	 * Costruttore vuoto. 
+	 * Ora la creazione delle stanze è delegata al LabirintoBuilder!
 	 */
 	public Labirinto() {
-		this.creaStanze();
-		this.stanzaCorrente = this.getStanzaIniziale();
+		// Niente più creaStanze() qui dentro!
 	}
 
 	/**
-     * Crea tutte le stanze e le porte di collegamento
-     */
-    private void creaStanze() {
-
-		/* crea gli attrezzi */
-    	Attrezzo lanterna = new Attrezzo("lanterna",3);
-		Attrezzo osso = new Attrezzo("osso",1);
-    	
-		/* crea stanze del labirinto */
-		Stanza atrio = new Stanza("Atrio");
-		Stanza aulaN11 = new Stanza("Aula N11");
-		Stanza aulaN10 = new Stanza("Aula N10");
-		Stanza laboratorio = new Stanza("Laboratorio Campus");
-		Stanza biblioteca = new Stanza("Biblioteca");
-		
-		/* collega le stanze */
-		atrio.impostaStanzaAdiacente("nord", biblioteca);
-		atrio.impostaStanzaAdiacente("est", aulaN11);
-		atrio.impostaStanzaAdiacente("sud", aulaN10);
-		atrio.impostaStanzaAdiacente("ovest", laboratorio);
-		aulaN11.impostaStanzaAdiacente("est", laboratorio);
-		aulaN11.impostaStanzaAdiacente("ovest", atrio);
-		aulaN10.impostaStanzaAdiacente("nord", atrio);
-		aulaN10.impostaStanzaAdiacente("est", aulaN11);
-		aulaN10.impostaStanzaAdiacente("ovest", laboratorio);
-		laboratorio.impostaStanzaAdiacente("est", atrio);
-		laboratorio.impostaStanzaAdiacente("ovest", aulaN11);
-		biblioteca.impostaStanzaAdiacente("sud", atrio);
-
-        /* pone gli attrezzi nelle stanze */
-		aulaN10.addAttrezzo(lanterna);
-		atrio.addAttrezzo(osso);
-
-		// il gioco comincia nell'atrio
-		this.stanzaIniziale = atrio;
-        this.stanzaVincente = biblioteca;;
-    }
-
-	public void setStanzaCorrente(Stanza stanzaCorrente) {
-		this.stanzaCorrente = stanzaCorrente;
-	}
-
-	public Stanza getStanzaCorrente() {
-		return this.stanzaCorrente;
-	}
-
-	public Stanza getStanzaVincente() {
-		return stanzaVincente;
+	 * Metodo statico per richiamare comodamente il costruttore (Builder)
+	 */
+	public static LabirintoBuilder newBuilder() {
+		return new LabirintoBuilder();
 	}
 
 	public Stanza getStanzaIniziale() {
@@ -73,6 +26,10 @@ public class Labirinto {
 
 	public void setStanzaIniziale(Stanza stanzaIniziale) {
 		this.stanzaIniziale = stanzaIniziale;
+	}
+
+	public Stanza getStanzaVincente() {
+		return stanzaVincente;
 	}
 
 	public void setStanzaVincente(Stanza stanzaVincente) {
